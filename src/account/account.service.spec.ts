872b83c907ea9a544fbd919819ea6810c7a74737
Dennis Ploeger <dennis.ploeger@kps.com>
2020-10-17 21:56:36 +0200

    feat: Completed account service

--- src/account/account.service.spec.ts
+++ src/account/account.service.spec.ts
@@ -3,8 +3,10 @@ import { AccountService } from './account.service';
 import { ConfigModule } from '@nestjs/config';
 import { startContainer, stopContainer } from '../../test/docker-ldap';
 import * as winston from 'winston';
-import * as logform from 'logform';
 import { setupLogger } from '../../test/setup-logger';
+import { AliasDoesNotExistError } from '../errors/alias-does-not-exist.error';
+import { AliasAlreadyExistsError } from '../errors/alias-already-exists.error';
+import { AccountInvalidError } from '../errors/account-invalid.error';
 
 describe('AccountService', () => {
   let service: AccountService;
@@ -50,5 +52,97 @@ describe('AccountService', () => {
     expect(aliases[0]).toStrictEqual({
       address: 'alias1.user@example.com',
     });
+  });
+
+  it('should throw when giving a non existent user', async () => {
+    try {
+      const aliases = await service.getAliases('unknown');
+    } catch (e) {
+      expect(e.name).toEqual(AccountInvalidError.NAME);
+    }
+  });
+
+  it('should create a new alias', async () => {
+    let aliases = await service.getAliases('user');
+    expect(aliases).toHaveLength(1);
+    const newAlias = await service.createAlias('user', {
+      address: 'alias2.user@example.com',
+    });
+    expect(newAlias).not.toBeNull();
+    expect(newAlias).toStrictEqual({
+      address: 'alias2.user@example.com',
+    });
+    aliases = await service.getAliases('user');
+    expect(aliases).toHaveLength(2);
+    expect(aliases[1]).toStrictEqual({
+      address: 'alias2.user@example.com',
+    });
+  });
+
+  it('should update an alias', async () => {
+    let aliases = await service.getAliases('user');
+    expect(aliases).toHaveLength(1);
+    const updatedAlias = await service.updateAlias(
+      'user',
+      'alias1.user@example.com',
+      {
+        address: 'alias2.user@example.com',
+      },
+    );
+    expect(updatedAlias).not.toBeNull();
+    expect(updatedAlias).toStrictEqual({
+      address: 'alias2.user@example.com',
+    });
+    aliases = await service.getAliases('user');
+    expect(aliases).toHaveLength(1);
+    expect(aliases[0]).toStrictEqual({
+      address: 'alias2.user@example.com',
+    });
+  });
+
+  it('should delete an alias', async () => {
+    let aliases = await service.getAliases('user');
+    expect(aliases).toHaveLength(1);
+    await service.deleteAlias('user', 'alias1.user@example.com');
+    aliases = await service.getAliases('user');
+    expect(aliases).toHaveLength(0);
+  });
+
+  it('should refuse adding an already existing alias', async () => {
+    try {
+      await service.createAlias('user', {
+        address: 'alias1.user@example.com',
+      });
+    } catch (e) {
+      expect(e.name).toEqual(AliasAlreadyExistsError.NAME);
+    }
+  });
+
+  it('should refuse changing an alias that does not exist', async () => {
+    try {
+      await service.updateAlias('user', 'notexists@example.com', {
+        address: 'alias1.user@example.com',
+      });
+    } catch (e) {
+      expect(e.name).toEqual(AliasDoesNotExistError.NAME);
+    }
+  });
+
+  it('should refuse changing an alias to an alias that already exists', async () => {
+    try {
+      await service.updateAlias('user', 'alias1.user@example.com', {
+        address: 'alias1.user@example.com',
+      });
+    } catch (e) {
+      expect(e.name).toEqual(AliasAlreadyExistsError.NAME);
+    }
+  });
+
+  it('should refuse deleting an alias that does not exist', async () => {
+    try {
+      await service.deleteAlias('user', 'notexists@example.com');
+    } catch (e) {
+      expect(e.name).toEqual(AliasDoesNotExistError.NAME);
+    }
   });
 });
--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AliasDto } from '../dto/alias.dto';
-import { Client } from 'ldapts';
+import { Attribute, Change, Client } from 'ldapts';
 import { AccountInvalidError } from '../errors/account-invalid.error';
 import { Entry } from 'ldapts/messages';
 import { Configuration } from '../configuration';
-import { CantConnectToLdapError } from '../errors/cant-connect-to-ldap.error';
 import * as winston from 'winston';
+import { AliasAlreadyExistsError } from '../errors/alias-already-exists.error';
+import { AliasDoesNotExistError } from '../errors/alias-does-not-exist.error';
 
+/**
+ * Alias and account modification service
+ */
 @Injectable()
 export class AccountService {
   private _client: Client;
@@ -23,6 +27,10 @@ export class AccountService {
     });
   }
 
+  /**
+   * Bind to ldap server
+   * @private
+   */
   private async _bind() {
     if (!this._bound) {
       const bindDn = this.configService.get('AM_LDAP_BIND_DN');
@@ -35,8 +43,13 @@ export class AccountService {
     }
   }
 
+  /**
+   * Fetch account object from ldap server
+   * @param username Username of account
+   */
   async _getAccount(username: string): Promise<Entry> {
     await this._bind();
+    winston.info(`Getting account ${username}`);
 
     const filter = `(${this._configService.get(
       'AM_LDAP_USER_ATTR',
@@ -55,11 +68,19 @@ export class AccountService {
     return results.searchEntries[0];
   }
 
+  /**
+   * Retrieve all aliases of user
+   * @param username User account
+   * @param filter Filter to search for aliases
+   */
   async getAliases(username: string, filter = ''): Promise<AliasDto[]> {
     const account = await this._getAccount(username);
     let aliases = account[this._configService.get('AM_LDAP_ALIAS_ATTR')] as
       | string
       | string[];
+    if (aliases === undefined) {
+      return [];
+    }
     if (!(aliases instanceof Array)) {
       aliases = [aliases];
     }
@@ -70,19 +91,103 @@ export class AccountService {
       });
   }
 
-  async createAlias(username: string, alias: AliasDto): Promise<AliasDto[]> {
-    return null;
+  /**
+   * Add a new alias to the account
+   * @param username Account name
+   * @param alias Alias to add
+   */
+  async createAlias(username: string, alias: AliasDto): Promise<AliasDto> {
+    winston.info(`Adding ${alias.address} to user ${username}`);
+    winston.info('Checking for duplicates');
+    if ((await this.getAliases(username, alias.address)).length > 0) {
+      throw new AliasAlreadyExistsError(username, alias);
+    }
+    const account = await this._getAccount(username);
+
+    winston.info('Adding alias');
+    await this._client.modify(
+      account.dn,
+      new Change({
+        operation: 'add',
+        modification: new Attribute({
+          type: this.configService.get('AM_LDAP_ALIAS_ATTR'),
+          values: [alias.address],
+        }),
+      }),
+    );
+
+    return alias;
   }
 
+  /**
+   * Change an alias
+   * @param username The user to modify
+   * @param address The original alias
+   * @param newAlias The new alias
+   */
   async updateAlias(
     username: string,
     address: string,
     newAlias: AliasDto,
-  ): Promise<void> {
-    return null;
+  ): Promise<AliasDto> {
+    winston.info(
+      `Changing ${address} to ${newAlias.address} for user ${username}`,
+    );
+    winston.info('Getting existing aliases');
+    const aliases = await this.getAliases(username);
+    if (!aliases.some(alias => alias.address === address)) {
+      throw new AliasDoesNotExistError(username, address);
+    }
+    if (aliases.some(alias => alias.address === newAlias.address)) {
+      throw new AliasAlreadyExistsError(username, newAlias);
+    }
+    const account = await this._getAccount(username);
+
+    winston.info('Changing alias');
+    await this._client.modify(account.dn, [
+      new Change({
+        operation: 'delete',
+        modification: new Attribute({
+          type: this.configService.get('AM_LDAP_ALIAS_ATTR'),
+          values: [address],
+        }),
+      }),
+
+      new Change({
+        operation: 'add',
+        modification: new Attribute({
+          type: this.configService.get('AM_LDAP_ALIAS_ATTR'),
+          values: [newAlias.address],
+        }),
+      }),
+    ]);
+
+    return newAlias;
   }
 
+  /**
+   * Delete an alias
+   * @param username Account to modify
+   * @param address Alias to delete
+   */
   async deleteAlias(username: string, address: string): Promise<void> {
-    return null;
+    winston.info(`Deleting ${address} from user ${username}`);
+    if ((await this.getAliases(username, address)).length == 0) {
+      throw new AliasDoesNotExistError(username, address);
+    }
+
+    const account = await this._getAccount(username);
+
+    winston.info('Deleting alias');
+    await this._client.modify(
+      account.dn,
+      new Change({
+        operation: 'delete',
+        modification: new Attribute({
+          type: this.configService.get('AM_LDAP_ALIAS_ATTR'),
+          values: [address],
+        }),
+      }),
+    );
   }
 }
